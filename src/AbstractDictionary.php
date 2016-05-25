5b727aa77309c393b60dda48dda544b0b9e3d9a7
Benjamin Bruska <benjamin.bruska@fortuneglobe.com>
2016-05-25 10:56:37 +0200

    add abstract dictionary

---
+++ src/AbstractDictionary.php
@@ -0,0 +1,311 @@
+<?php
+namespace Hansel23\Dictionaries;
+
+use Hansel23\Dictionaries\Exceptions\ArgumentException;
+use Hansel23\Dictionaries\Exceptions\InvalidDictionaryException;
+use Hansel23\Dictionaries\Exceptions\InvalidKeyException;
+use Hansel23\Dictionaries\Exceptions\InvalidTypeException;
+use Hansel23\Dictionaries\Interfaces\RegistersItems;
+
+/**
+ * Class AbstractDictionary
+ *
+ * @package Hansel23\Dictionaries
+ */
+abstract class AbstractDictionary implements RegistersItems
+{
+	/**
+	 * @var string
+	 */
+	protected $dictionaryType;
+
+	/**
+	 * @var array
+	 */
+	protected $keyMap = [ ];
+
+	/**
+	 * @var array
+	 */
+	protected $keyValuePairs = [ ];
+
+	/**
+	 * @var int
+	 */
+	protected $currentPosition = 0;
+
+	/**
+	 * @var int
+	 */
+	protected $itemCount = 0;
+
+	/**
+	 * AbstractDictionary constructor.
+	 */
+	public function __construct()
+	{
+		$this->dictionaryType = get_class( $this );
+	}
+
+	/**
+	 * @return string
+	 */
+	abstract protected function getNameOfKeyType();
+
+	/**
+	 * @return string
+	 */
+	abstract protected function getNameOfValueType();
+
+	/**
+	 * @param $key
+	 * @param $value
+	 *
+	 * @throws ArgumentException
+	 */
+	public function add( $key, $value )
+	{
+		if ( $this->offsetExists( $key ) )
+		{
+			throw new ArgumentException( 'Duplicate key: ' . $this->stringifyKey( $key ) );
+		}
+
+		$this->offsetSet( $key, $value );
+	}
+
+	/**
+	 * @param RegistersItems $registersItems
+	 *
+	 * @throws InvalidDictionaryException
+	 */
+	public function merge( RegistersItems $registersItems )
+	{
+		$dictionaryTypeToMergeWith = get_class( $registersItems );
+
+		if ( $dictionaryTypeToMergeWith != $this->dictionaryType )
+		{
+			throw new InvalidDictionaryException(
+				sprintf(
+					'Invalid dictionary-type: %s. Expected: %s', $dictionaryTypeToMergeWith, $this->dictionaryType
+				)
+			);
+		}
+
+		foreach ( $registersItems as $key => $value )
+		{
+			$this->offsetSet( $key, $value );
+		}
+	}
+
+	/**
+	 * @param mixed $offset
+	 *
+	 * @return bool
+	 * */
+	public function offsetExists( $offset )
+	{
+		return $this->existsStringifiedKey( $this->stringifyKey( $offset ) );
+	}
+
+	/**
+	 * @param mixed $offset
+	 *
+	 * @return KeyValuePair
+	 * @throws InvalidKeyException
+	 */
+	public function offsetGet( $offset )
+	{
+		$stringifiedKey = $this->stringifyKey( $offset );
+
+		$this->ensureStringifiedKeyDoesExist( $stringifiedKey );
+
+		return $this->keyValuePairs[ $this->keyMap[ $stringifiedKey ] ]->getValue();
+	}
+
+	/**
+	 * @param mixed $offset
+	 * @param mixed $value
+	 *
+	 * @throws ArgumentException
+	 */
+	public function offsetSet( $offset, $value )
+	{
+		$this->validateKeyType( $offset );
+		$this->validateValueType( $value );
+
+		$stringifiedKey = $this->stringifyKey( $offset );
+
+		if ( !array_key_exists( $stringifiedKey, $this->keyMap ) )
+		{
+			$this->keyMap[ $stringifiedKey ] = $this->itemCount++;
+		}
+
+		$this->keyValuePairs[ $this->keyMap[ $stringifiedKey ] ] = new KeyValuePair( $offset, $value );
+	}
+
+	/**
+	 * @param mixed $offset
+	 *
+	 * @throws InvalidKeyException
+	 */
+	public function offsetUnset( $offset )
+	{
+		$stringifiedKey = $this->stringifyKey( $offset );
+
+		$this->ensureStringifiedKeyDoesExist( $stringifiedKey );
+
+		unset($this->keyValuePairs[ $this->keyMap[ $stringifiedKey ] ]);
+		unset($this->keyMap[ $stringifiedKey ]);
+
+		$this->keyMap = array_flip( array_keys( $this->keyMap ) );
+		$this->keyValuePairs = array_values( $this->keyValuePairs );
+
+		$this->itemCount--;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function count()
+	{
+		return $this->itemCount;
+	}
+
+	/**
+	 * @return bool|mixed
+	 */
+	public function current()
+	{
+		if ( isset($this->keyValuePairs[ $this->currentPosition ]) )
+		{
+			return $this->keyValuePairs[ $this->currentPosition ]->getValue();
+		}
+
+		return false;
+	}
+
+	/**
+	 * @return void
+	 */
+	public function next()
+	{
+		$this->currentPosition++;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function key()
+	{
+		return $this->keyValuePairs[ $this->currentPosition ]->getKey();
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function valid()
+	{
+		return isset($this->keyValuePairs[ $this->currentPosition ]);
+	}
+
+	/**
+	 * @return void
+	 */
+	public function rewind()
+	{
+		$this->currentPosition = 0;
+	}
+
+	/**
+	 * @param $key
+	 *
+	 * @throws InvalidTypeException
+	 */
+	private function validateKeyType( $key )
+	{
+		$this->validateType( $key, $this->getNameOfKeyType() );
+	}
+
+	/**
+	 * @param $value
+	 *
+	 * @throws InvalidTypeException
+	 */
+	private function validateValueType( $value )
+	{
+		$this->validateType( $value, $this->getNameOfValueType() );
+	}
+
+	/**
+	 * @param mixed $typeToValidate
+	 * @param string $validTypeName
+	 *
+	 * @throws InvalidTypeException
+	 */
+	private function validateType( $typeToValidate, $validTypeName )
+	{
+		if( is_object( $typeToValidate ) )
+		{
+			$typeName = get_class( $typeToValidate );
+		}
+		else
+		{
+			$typeName = gettype( $typeToValidate );
+		}
+
+		if ( $typeName !== $validTypeName )
+		{
+			throw new InvalidTypeException(
+				sprintf( 'Invalid element of type %s, expected: %s', $typeName, $validTypeName )
+			);
+		}
+	}
+
+	/**
+	 * @param $key
+	 *
+	 * @return string
+	 * @throws InvalidTypeException
+	 */
+	private function stringifyKey( $key )
+	{
+		if ( is_scalar( $key ) )
+		{
+			return md5( sprintf( '%s:%s', gettype( $key ), $key ) );
+		}
+		else
+		{
+			try
+			{
+				return md5( serialize( $key ) );
+			}
+			catch ( \Exception $ex )
+			{
+				throw new InvalidTypeException( 'Unserializeable key given' );
+			}
+		}
+	}
+
+	/**
+	 * @param $key
+	 *
+	 * @return bool
+	 */
+	private function existsStringifiedKey( $key )
+	{
+		return array_key_exists( $key, $this->keyMap );
+	}
+
+	/**
+	 * @param $key
+	 *
+	 * @throws InvalidKeyException
+	 */
+	private function ensureStringifiedKeyDoesExist( $key )
+	{
+		if ( !$this->existsStringifiedKey( $key ) )
+		{
+			throw new InvalidKeyException( $key );
+		}
+	}
+}
--- src/Dictionary.php
+++ src/Dictionary.php
@@ -1,10 +1,6 @@
 <?php
 namespace Hansel23\Dictionaries;
 
-use Hansel23\Dictionaries\Exceptions\ArgumentException;
-use Hansel23\Dictionaries\Exceptions\InvalidDictionaryException;
-use Hansel23\Dictionaries\Exceptions\InvalidKeyException;
-use Hansel23\Dictionaries\Exceptions\InvalidTypeException;
 use Hansel23\Dictionaries\Interfaces\RegistersItems;
 
 /**
@@ -12,7 +8,7 @@ use Hansel23\Dictionaries\Interfaces\RegistersItems;
  *
  * @package Hansel23\Dictionaries
  */
-class Dictionary implements RegistersItems
+class Dictionary extends AbstractDictionary implements RegistersItems
 {
 	/**
 	 * @var string
@@ -25,292 +21,30 @@ class Dictionary implements RegistersItems
 	protected $validValueTypeName;
 
 	/**
-	 * @var string
-	 */
-	protected $dictionaryType;
-
-	/**
-	 * @var array
-	 */
-	protected $keyMap = [ ];
-
-	/**
-	 * @var array
-	 */
-	protected $keyValuePairs = [ ];
-
-	/**
-	 * @var int
-	 */
-	protected $currentPosition = 0;
-
-	/**
-	 * @var int
-	 */
-	protected $itemCount = 0;
-
-	/**
 	 * @param string $keyClassName
 	 * @param string $valueClassName
 	 */
 	public function __construct( $keyClassName, $valueClassName )
 	{
-		$this->validKeyTypeName = $keyClassName;
-
+		$this->validKeyTypeName   = $keyClassName;
 		$this->validValueTypeName = $valueClassName;
 
-		$this->dictionaryType = get_class( $this );
+		parent::__construct();
 	}
 
 	/**
-	 * @param $key
-	 * @param $value
-	 *
-	 * @throws ArgumentException
-	 */
-	public function add( $key, $value )
-	{
-		if ( $this->offsetExists( $key ) )
-		{
-			throw new ArgumentException( 'Duplicate key: ' . $this->stringifyKey( $key ) );
-		}
-
-		$this->offsetSet( $key, $value );
-	}
-
-	/**
-	 * @param RegistersItems $registersItems
-	 *
-	 * @throws InvalidDictionaryException
-	 */
-	public function merge( RegistersItems $registersItems )
-	{
-		$dictionaryTypeToMergeWith = get_class( $registersItems );
-
-		if ( $dictionaryTypeToMergeWith != $this->dictionaryType )
-		{
-			throw new InvalidDictionaryException(
-				sprintf(
-					'Invalid dictionary-type: %s. Expected: %s', $dictionaryTypeToMergeWith, $this->dictionaryType
-				)
-			);
-		}
-
-		foreach ( $registersItems as $key => $value )
-		{
-			$this->offsetSet( $key, $value );
-		}
-	}
-
-	/**
-	 * @param mixed $offset
-	 *
-	 * @return bool
-	 * */
-	public function offsetExists( $offset )
-	{
-		return $this->existsStringifiedKey( $this->stringifyKey( $offset ) );
-	}
-
-	/**
-	 * @param mixed $offset
-	 *
-	 * @return KeyValuePair
-	 * @throws InvalidKeyException
-	 */
-	public function offsetGet( $offset )
-	{
-		$stringifiedKey = $this->stringifyKey( $offset );
-
-		$this->ensureStringifiedKeyDoesExist( $stringifiedKey );
-
-		return $this->keyValuePairs[ $this->keyMap[ $stringifiedKey ] ]->getValue();
-	}
-
-	/**
-	 * @param mixed $offset
-	 * @param mixed $value
-	 *
-	 * @throws ArgumentException
-	 */
-	public function offsetSet( $offset, $value )
-	{
-		$this->validateKeyType( $offset );
-		$this->validateValueType( $value );
-
-		$stringifiedKey = $this->stringifyKey( $offset );
-
-		if ( !array_key_exists( $stringifiedKey, $this->keyMap ) )
-		{
-			$this->keyMap[ $stringifiedKey ] = $this->itemCount++;
-		}
-
-		$this->keyValuePairs[ $this->keyMap[ $stringifiedKey ] ] = new KeyValuePair( $offset, $value );
-	}
-
-	/**
-	 * @param mixed $offset
-	 *
-	 * @throws InvalidKeyException
-	 */
-	public function offsetUnset( $offset )
-	{
-		$stringifiedKey = $this->stringifyKey( $offset );
-
-		$this->ensureStringifiedKeyDoesExist( $stringifiedKey );
-
-		unset($this->keyValuePairs[ $this->keyMap[ $stringifiedKey ] ]);
-		unset($this->keyMap[ $stringifiedKey ]);
-
-		$this->keyMap = array_flip( array_keys( $this->keyMap ) );
-		$this->keyValuePairs = array_values( $this->keyValuePairs );
-
-		$this->itemCount--;
-	}
-
-	/**
-	 * @return int
-	 */
-	public function count()
-	{
-		return $this->itemCount;
-	}
-
-	/**
-	 * @return bool|mixed
-	 */
-	public function current()
-	{
-		if ( isset($this->keyValuePairs[ $this->currentPosition ]) )
-		{
-			return $this->keyValuePairs[ $this->currentPosition ]->getValue();
-		}
-
-		return false;
-	}
-
-	/**
-	 * @return void
-	 */
-	public function next()
-	{
-		$this->currentPosition++;
-	}
-
-	/**
-	 * @return mixed
-	 */
-	public function key()
-	{
-		return $this->keyValuePairs[ $this->currentPosition ]->getKey();
-	}
-
-	/**
-	 * @return bool
-	 */
-	public function valid()
-	{
-		return isset($this->keyValuePairs[ $this->currentPosition ]);
-	}
-
-	/**
-	 * @return void
-	 */
-	public function rewind()
-	{
-		$this->currentPosition = 0;
-	}
-
-	/**
-	 * @param $key
-	 *
-	 * @throws InvalidTypeException
-	 */
-	private function validateKeyType( $key )
-	{
-		$this->validateType( $key, $this->validKeyTypeName );
-	}
-
-	/**
-	 * @param $value
-	 *
-	 * @throws InvalidTypeException
-	 */
-	private function validateValueType( $value )
-	{
-		$this->validateType( $value, $this->validValueTypeName );
-	}
-
-	/**
-	 * @param mixed $typeToValidate
-	 * @param string $validTypeName
-	 *
-	 * @throws InvalidTypeException
-	 */
-	private function validateType( $typeToValidate, $validTypeName )
-	{
-		if( is_object( $typeToValidate ) )
-		{
-			$typeName = get_class( $typeToValidate );
-		}
-		else
-		{
-			$typeName = gettype( $typeToValidate );
-		}
-
-		if ( $typeName !== $validTypeName )
-		{
-			throw new InvalidTypeException(
-				sprintf( 'Invalid element of type %s, expected: %s', $typeName, $validTypeName )
-			);
-		}
-	}
-
-	/**
-	 * @param $key
-	 *
 	 * @return string
-	 * @throws InvalidTypeException
 	 */
-	private function stringifyKey( $key )
+	protected function getNameOfKeyType()
 	{
-		if ( is_scalar( $key ) )
-		{
-			return md5( sprintf( '%s:%s', gettype( $key ), $key ) );
-		}
-		else
-		{
-			try
-			{
-				return md5( serialize( $key ) );
-			}
-			catch ( \Exception $ex )
-			{
-				throw new InvalidTypeException( 'Unserializeable key given' ); 
-			}
-		}
+		return $this->validKeyTypeName;
 	}
 
 	/**
-	 * @param $key
-	 *
-	 * @return bool
+	 * @return string
 	 */
-	private function existsStringifiedKey( $key )
+	protected function getNameOfValueType()
 	{
-		return array_key_exists( $key, $this->keyMap );
-	}
-
-	/**
-	 * @param $key
-	 *
-	 * @throws InvalidKeyException
-	 */
-	private function ensureStringifiedKeyDoesExist( $key )
-	{
-		if ( !$this->existsStringifiedKey( $key ) )
-		{
-			throw new InvalidKeyException( $key );
-		}
+		return $this->validValueTypeName;
 	}
 }
